70698e47992ac9ee0b5bbec1926e22f79f876aa8
freejiebao <jiexiao@pku.edu.cn>
2018-10-22 15:04:01 +0200

    add crab jobs

---
+++ PTAnalysis/test/SubCrab/crab3_analysisDY_8.py
@@ -0,0 +1,24 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+config.section_("General")
+config.General.requestName   = 'DY-1'
+config.General.transferLogs = True
+
+config.section_("JobType")
+config.JobType.pluginName  = 'Analysis'
+#config.JobType.inputFiles = ['Summer16_23Sep2016V4_MC_L1FastJet_AK4PFchs.txt','Summer16_23Sep2016V4_MC_L2Relative_AK4PFchs.txt','Summer16_23Sep2016V4_MC_L3Absolute_AK4PFchs.txt','Summer16_23Sep2016V4_MC_L1FastJet_AK4PFPuppi.txt','Summer16_23Sep2016V4_MC_L2Relative_AK4PFPuppi.txt','Summer16_23Sep2016V4_MC_L3Absolute_AK4PFPuppi.txt']
+# Name of the CMSSW configuration file
+config.JobType.psetName    = 'electronIsolationAnalyzer_cfg.py'
+config.JobType.allowUndistributedCMSSW = True
+
+config.section_("Data")
+config.Data.inputDataset = '/DYToLL-M-50_2J_14TeV-madgraphMLM-pythia8/PhaseIISpr18AODMiniAOD-PU200_93X_upgrade2023_realistic_v5_ext1-v1/AODSIM'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 10
+config.Data.totalUnits = -1
+config.Data.publication = False
+config.Data.outputDatasetTag = 'DY-8'
+
+config.section_("Site")
+config.Site.storageSite = 'T2_CN_Beijing'  #2_CH_CERN'
