97cc0d08cfeca6a21be45ad33a138a13e7547a9f
freejiebao <jiexiao@pku.edu.cn>
2018-10-26 18:29:19 +0200

    add crab submit and status file

---
+++ PTAnalysis/test/status.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+crab status -d DY_noPU_TuneCUETP8M1
+crab status -d DY_noPU_0J
+crab status -d DY_noPU_1J
+crab status -d DY_noPU_2J
+crab status -d DY_noPU_3J
+crab status -d DY_PU200_TuneCUETP8M1_ext
+crab status -d DY_PU200_TuneCUETP8M1
+crab status -d DY_PU200_0J_ext
+crab status -d DY_PU200_0J
+crab status -d DY_PU200_1J_ext
+crab status -d DY_PU200_1J
+crab status -d DY_PU200_2J_ext
+crab status -d DY_PU200_2J
+crab status -d DY_PU200_3J
+crab status -d QCD_noPU
+crab status -d QCD_PU200_ext
+crab status -d QCD_PU200
---
+++ PTAnalysis/test/submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+crab submit -c ./crab/crab3_analysis_DY_noPU_v1.py
+crab submit -c ./crab/crab3_analysis_DY_noPU_v2.py
+crab submit -c ./crab/crab3_analysis_DY_noPU_v3.py
+crab submit -c ./crab/crab3_analysis_DY_noPU_v4.py
+crab submit -c ./crab/crab3_analysis_DY_noPU_v5.py
+crab submit -c ./crab/crab3_analysis_DY_PU200_v1_ext.py
+crab submit -c ./crab/crab3_analysis_DY_PU200_v1.py
+crab submit -c ./crab/crab3_analysis_DY_PU200_v2_ext.py
+crab submit -c ./crab/crab3_analysis_DY_PU200_v2.py
+crab submit -c ./crab/crab3_analysis_DY_PU200_v3_ext.py
+crab submit -c ./crab/crab3_analysis_DY_PU200_v3.py
+crab submit -c ./crab/crab3_analysis_DY_PU200_v4_ext.py
+crab submit -c ./crab/crab3_analysis_DY_PU200_v4.py
+crab submit -c ./crab/crab3_analysis_DY_PU200_v5.py
+crab submit -c ./crab/crab3_analysis_QCD_noPU_v1.py
+crab submit -c ./crab/crab3_analysis_QCD_PU200_v1.py
+crab submit -c ./crab/crab3_analysis_QCD_PU200_v1_ext.py
