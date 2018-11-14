b930da52d5d260d534fd4827f47d4a3ad79a1fb0
freejiebao <jiexiao@pku.edu.cn>
2018-11-14 14:49:23 +0800

    add resubmit file

---
+++ PTAnalysis/test/resubmit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+crab resubmit -d crab_DY_noPU_TuneCUETP8M1
+crab resubmit -d crab_DY_noPU_0J
+crab resubmit -d crab_DY_noPU_1J
+crab resubmit -d crab_DY_noPU_2J
+crab resubmit -d crab_DY_noPU_3J
+crab resubmit -d crab_DY_PU200_TuneCUETP8M1_ext
+crab resubmit -d crab_DY_PU200_TuneCUETP8M1
+crab resubmit -d crab_DY_PU200_0J_ext
+crab resubmit -d crab_DY_PU200_0J
+crab resubmit -d crab_DY_PU200_1J_ext
+crab resubmit -d crab_DY_PU200_1J
+crab resubmit -d crab_DY_PU200_2J_ext
+crab resubmit -d crab_DY_PU200_2J
+crab resubmit -d crab_DY_PU200_3J
+crab resubmit -d crab_QCD_noPU
+crab resubmit -d crab_QCD_PU200_ext
+crab resubmit -d crab_QCD_PU200
+crab resubmit -d crab_DY_PU200_GEN-SIM-RECO_ext
+crab resubmit -d crab_DY_PU200_GEN-SIM-RECO
+crab resubmit -d crab_DY_noPU_GEN-SIM-RECO
+crab resubmit -d crab_QCD_PU200_GEN-SIM-RECO_ext
+crab resubmit -d crab_QCD_PU200_GEN-SIM-RECO
+crab resubmit -d crab_QCD_noPU_GEN-SIM-RECO
